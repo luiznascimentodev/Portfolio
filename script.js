3e599c2992e679d45061ab8ce96c9257eccb65f0
luiznascimentodev <luiznascdev@gmail.com>
2024-11-19 10:47:10 -0300

    Atualizações CSS , inclusão scroll to top

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-// Selecione o botão do menu hambúrguer, a navbar e os links
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navbar = document.querySelector('.navbar');
+const hamburger = document.querySelector('.hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links a');
 
-// Adiciona um evento de clique no botão hambúrguer
-hamburgerMenu.addEventListener('click', () => {
-  // Alterna a classe 'active' para mostrar/ocultar os links
+hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+
+// Seleciona o botão
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+// Mostra o botão ao rolar para baixo
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) { // Exibe o botão após 200px de rolagem
+    scrollToTopButton.classList.add('visible');
+  } else {
+    scrollToTopButton.classList.remove('visible');
+  }
+});
+
+// Função para rolar até o topo ao clicar no botão
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Rola suavemente
+  });
+});
